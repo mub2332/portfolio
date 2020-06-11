43eb17cc48b3f84601423c02bc91b48a7bca3276
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-06-11 23:27:51 +1000

    Updates

---
+++ precache-manifest.98b60ddfdbb2fd170dbf8d507e433482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ffe84e6d5245f13e1d82c683a175dc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60fcaf22aa7b7928dc60",
+    "url": "/portfolio/static/css/main.889b05e7.chunk.css"
+  },
+  {
+    "revision": "07a43d13e099be46e7b6",
+    "url": "/portfolio/static/js/2.352914d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.352914d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fcaf22aa7b7928dc60",
+    "url": "/portfolio/static/js/main.faff00c5.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "2ef28eff5bdbcb0cb9ab1eb9da24067e",
+    "url": "/portfolio/static/media/bg.2ef28eff.jpg"
+  }
+]);
