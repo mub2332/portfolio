4d3f5228379eabdf1e97f0c5e4a801c76782b264
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-06-14 08:52:41 +1000

    Added sticky navbar

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './nav.module.scss';
 import { NavLink } from 'react-router-dom';
+import { useRef } from 'react';
 
 const Nav = () => {
+  const [styles, setStyles] = useState('transparent');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset === 0) {
+        setStyles('transparent');
+      } else {
+        setStyles('opaque');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [styles]);
+
   return (
-    <div className={s.navContainer}>
+    <div
+      className={
+        s.navContainer +
+        ' ' +
+        (styles === 'transparent' ? s.transparentNav : s.opaqueNav)
+      }
+    >
       <nav>
         <ul className={s.nav}>
           <li>
