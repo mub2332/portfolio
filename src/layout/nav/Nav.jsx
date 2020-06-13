39380a44f0bbb8fd703b574cf7a86ecf7ab87304
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-06-13 18:52:27 +1000

    Changed link hierarchy

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -1,52 +1,52 @@
-import React from "react";
-import s from "./nav.module.scss";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import s from './nav.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
-    return (
-        <div className={s.navContainer}>
-            <nav>
-                <ul className={s.nav}>
-                    <li>
-                        <NavLink
-                            exact={true}
-                            activeStyle={{
-                                color: "#c8a2c8",
-                                borderBottom: "2px solid #c8a2c8",
-                            }}
-                            to="/portfolio"
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            exact={true}
-                            activeStyle={{
-                                color: "#c8a2c8",
-                                borderBottom: "2px solid #c8a2c8",
-                            }}
-                            to="/skills"
-                        >
-                            Skills
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            exact={true}
-                            activeStyle={{
-                                color: "#c8a2c8",
-                                borderBottom: "2px solid #c8a2c8",
-                            }}
-                            to="/projects"
-                        >
-                            Projects
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div className={s.navContainer}>
+      <nav>
+        <ul className={s.nav}>
+          <li>
+            <NavLink
+              exact={true}
+              activeStyle={{
+                color: '#c8a2c8',
+                borderBottom: '2px solid #c8a2c8',
+              }}
+              to="/portfolio"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact={true}
+              activeStyle={{
+                color: '#c8a2c8',
+                borderBottom: '2px solid #c8a2c8',
+              }}
+              to="/portfolio/skills"
+            >
+              Skills
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact={true}
+              activeStyle={{
+                color: '#c8a2c8',
+                borderBottom: '2px solid #c8a2c8',
+              }}
+              to="/portfolio/projects"
+            >
+              Projects
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
