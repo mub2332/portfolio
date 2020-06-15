87e047eaa816e05c62b4db35e4e2cf8f69663de4
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-06-16 04:05:08 +1000

    Added projects section

--- src/components/card/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './card.module.scss';
-
-const Card = ({ children }) => {
-  return <div className={s.card}>{children}</div>;
-};
-
-export default Card;
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React from 'react';
+import s from './projects.module.scss';
+import Card from './Card';
+import seraph from '../../images/seraph.png';
+import glipher from '../../images/glipher.png';
+import hwa from '../../images/hwa.png';
+
+const projectData = [
+  {
+    image: glipher,
+    title: 'Glipher',
+    description:
+      'A Chrome extension to clip sections of YouTube videos and create animated GIFs from them',
+    links: [{ text: 'View Code', url: 'https://github.com/mub2332/glipher' }],
+    tags: ['jQuery', 'AWS', 'Serverless'],
+  },
+  {
+    image: hwa,
+    title: 'Monash HWA Calculator',
+    description:
+      'A Chrome extension to help honors students calculate their honors weighted average mark easily',
+    links: [{ text: 'View Code', url: 'https://github.com/mub2332/glipher' }],
+    tags: ['jQuery'],
+  },
+];
 
 const Projects = () => {
-    return <div></div>;
+  return (
+    <div className={s.container}>
+      {projectData.map((project) => (
+        <Card {...project} />
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
