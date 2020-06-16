df874cd226d1040edf055843f624211952452d2b
Mubtasim Mahmud <mubtasim.mahmud@monash.edu>
2020-06-16 10:44:49 +1000

    Removed react-pdf, using image instead

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,41 +1,28 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf/dist/entry.webpack';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-
-import resume from './resume.pdf';
-import s from './resume.module.scss';
+import React, { useState } from "react";
+import resumeImage from "../../images/resume.png";
+import resume from "./resume.pdf";
+import s from "./resume.module.scss";
 
 const Resume = () => {
-  const download = () => {
-    const link = document.createElement('a');
-    link.href = resume;
-    link.download = 'Resume.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
+    const download = () => {
+        const link = document.createElement("a");
+        link.href = resume;
+        link.download = "Resume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
 
-  return (
-    <>
-      <Document
-        renderMode={'svg'}
-        className={s.container}
-        file={resume}
-        onLoadError={console.error}
-      >
-        <Page
-          renderAnnotationLayer={false}
-          className={s.page}
-          width={window.innerWidth * 0.7}
-          renderMode="svg"
-          pageNumber={1}
-        />
-      </Document>
-      <div className={s.center}>
-        <button onClick={download}>Download my resume</button>
-      </div>
-    </>
-  );
+    return (
+        <div className={s.container}>
+            <div className={s.center}>
+                <img className={s.image} src={resumeImage} />
+            </div>
+            <div className={s.center}>
+                <button onClick={download}>Download my resume</button>
+            </div>
+        </div>
+    );
 };
 
 export default Resume;
