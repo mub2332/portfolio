5a03f2907fa7cd0001641749f6af12d6333161bb
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2020-06-18 01:56:11 +1000

    Updated Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
     <div className={s.header}>
       <section className={s.headerText}>
         <h1 className={s.title}>Mubtasim Mahmud</h1>
-        <h1 className={s.subtitle}>Fullstack Web Developer</h1>
+        <h1 className={s.subtitle}>Full Stack Developer</h1>
       </section>
       <section className={s.about}>
         <h2 className={s.header}>About Me</h2>
         <p className={s.content}>
-          I have experience in creating full stack web apps using a variety of
-          web technologies. I consider myself to be language agnostic, and am
-          used to learning a new language or framework on the job.
+          I have experience in creating full stack apps using a variety of web
+          technologies. I consider myself to be language agnostic, and am used
+          to learning a new language or framework on the job.
           <br />
           <br />I like to upskill myself regularly with emerging technologies
           such as PWAs, GraphQL, Natural Language Processing and hybrid-native
